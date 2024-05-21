afc1df99484d2d2760030411c3cf7953e6c20a23
GITKLM <cprioton@gmail.com>
2024-05-21 18:02:31 +0400

    other components and Images

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import Engagement from "./components/Engagement";
+import Hero from "./components/Hero";
+import LeKapNumerik from "./components/LeKapNumerik";
+import Offres from "./components/Offres";
+
+
+
 
 export default function Home() {
   return (
-<main>
-  Hello
+<main className="">
+<Hero />
+<Engagement />
+<Offres />
+
+<LeKapNumerik />
 </main>
   );
 }
